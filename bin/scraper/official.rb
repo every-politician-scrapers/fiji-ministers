27b777c1770688c79cf707d88725b6bccade996c
Tony Bowden <tony@tmtm.com>
2021-08-23 11:33:50 +0300

    add scraper for official site

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -6,18 +6,25 @@ require 'pry'
 
 class MemberList
   class Member
+    # Only appears in the image, but thankfully is also in the URL of it
     def name
-      noko.css('.name').text.tidy
+      img.split('/').last.delete_prefix('HON.-').split('-300').first.gsub('-', ' ')
     end
 
     def position
-      noko.css('.position').text.tidy
+      noko.parent.parent.css('.wpb_content_element p').first.text.tidy.split(/ and (?=Minister)/).map(&:tidy)
+    end
+
+    private
+
+    def img
+      noko.css('img/@src').text.tidy
     end
   end
 
   class Members
     def member_container
-      noko.css('.member')
+      noko.css('.the_content .vc_figure')
     end
   end
 end
