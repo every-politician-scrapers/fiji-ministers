e08e633cd8f1c7409948a1398886b4dc5105f899
Tony Bowden <tony@tmtm.com>
2021-08-23 12:05:30 +0300

    Split some positions
    Wikidata treats these as separate positions, so break them up in the
    scraper.

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -6,16 +6,29 @@ require 'pry'
 
 class MemberList
   class Member
+    POSITION_MAP = {
+      'Minister for iTaukei Affairs, Sugar Industry and Foreign Affairs' => [
+        'Minister for iTaukei Affairs', 'Minister for Sugar Industry', 'Minister for Foreign Affairs'
+      ],
+      'Minister for Economy, Civil Service and Communications'           => [
+        'Minister for Economy', 'Minister for Civil Service', 'Minister for Communications'
+      ],
+    }.freeze
+
     # Only appears in the image, but thankfully is also in the URL of it
     def name
       img.split('/').last.delete_prefix('HON.-').split('-300').first.gsub('-', ' ')
     end
 
     def position
-      noko.parent.parent.css('.wpb_content_element p').first.text.tidy.split(/ and (?=Minister)/).map(&:tidy)
+      raw_position.flat_map { |posn| POSITION_MAP.fetch(posn, posn) }
     end
 
     private
+
+    def raw_position
+      noko.parent.parent.css('.wpb_content_element p').first.text.tidy.split(/ and (?=Minister)/).map(&:tidy)
+    end
 
     def img
       noko.css('img/@src').text.tidy
